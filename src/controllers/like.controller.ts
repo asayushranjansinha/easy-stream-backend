4d7b3eb64634b3a26d5bbdb9e649ac1aa4391ee2
ayush ranjan <asayushranjansinha@gmail.com>
2024-01-20 17:46:44 +0530

    completed get liked videos controller

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { isValidObjectId } from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import { LikeInstance } from "../models/like.model"
 import { ApiError } from "../utils/ApiError"
 import { ApiResponse } from "../utils/ApiResponse"
@@ -50,7 +50,81 @@ const toggleTweetLike = asyncHandler(async (req: Request, res: Response) => {
 )
 
 const getLikedVideos = asyncHandler(async (req: Request, res: Response) => {
-    //TODO: get all liked videos
+    // Extracting user from request user
+    const userId = new mongoose.Types.ObjectId(req.user?._id);
+
+    // Aggregation pipeline to fetch liked videos
+    const likedVideos = await LikeInstance.aggregate([
+        // Stage 1: Match documents where likedBy field is equal to userId
+        {
+            $match: {
+                likedBy: userId,
+            }
+        },
+        // Stage 2: Perform a lookup to get video details based on the video field
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner"
+                        }
+                    },
+                    {
+                        $unwind: "$owner"
+                    }
+                ]
+            }
+        },
+        // Stage 3: Unwind the video array to get individual video documents
+        {
+            $unwind: "$video"
+        },
+        // Stage 4: Perform a lookup to get likes details based on the video _id field
+        {
+            $lookup: {
+                from: "likes",
+                localField: "video._id",
+                foreignField: "video",
+                as: "video.likes"
+            }
+        },
+        // Stage 5: Add a new field 'likes' to the video document representing the count of likes
+        {
+            $addFields: {
+                "video.likes": {
+                    $size: "$video.likes"
+                }
+            }
+        },
+        // Stage 6: Project to include only the desired fields in the final output
+        {
+            $project: {
+                _id: "$video._id",
+                videoFile: "$video.videoFile",
+                thumbnail: "$video.thumbnail",
+                title: "$video.title",
+                description: "$video.description",
+                duration: "$video.duration",
+                views: "$video.views",
+                owner: "$video.owner.fullname",
+                createdAt: "$video.createdAt",
+                likes: "$video.likes"
+            }
+        }
+    ]);
+
+
+    // Sending the response
+    return res.status(200)
+        .json(new ApiResponse(200, likedVideos, "Fetched Liked videos"));
 })
 
 export {
